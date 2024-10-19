e13d54f36fc3634705b1f5503a2c460577718b5f
Guruganeshkannan <guruganeshkannan16@gmail.com>
2024-10-19 12:16:31 +0530

    Update dashboard_js.js

--- Script/dashboard_js.js
+++ Script/dashboard_js.js
@@ -3,44 +3,44 @@
         name: "Dr.Birmohan Singh",
         position: "Professor",
         expertise: "Ph.D., M.E",
-        imageUrl: "https://imgbb.com/][img]https://i.ibb.co/7tB0P4n/Birmohan-sing.jpg",
+        imageUrl: "https://i.ibb.co/7tB0P4n/Birmohan-sing.jpg",
         availableSlots: ["10:00 AM", "2:00 PM", "4:00 PM"]
     }, {
         name: "Dr. Damanpreet Singh",
         position: "Professor",
         expertise: "Ph.D., M.Tech,B.tech",
-        imageUrl: "https://i.ibb.co/7tB0P4n/Birmohan-sing.jpg",
+        imageUrl: "https://i.ibb.co/wJn3kd9/Dr-Damanpreet-Singh.jpg",
         availableSlots: ["10:00 AM", "2:00 PM", "4:00 PM"]
     }, {
         name: "Dr. Major Singh Goraya",
         position: "Professor",
         expertise: "Ph.D., M.Tech., B.Tech.",
-        imageUrl: "http://sliet.ac.in/wp-content/uploads/avatars/35/5e69c6be20c7e-bpfull.jpg",
+        imageUrl: "https://i.ibb.co/3f0hFgN/Dr-Manoj-Sachan.jpg",
         availableSlots: ["9:00 AM", "1:00 PM", "3:00 PM"]
     },{
         name: "Dr. Manoj Sachan",
         position: "Professor",
         expertise: "B.Tech(Computer Science) , M.E(Computer Science), Ph.D (Computer Science & Engineering)",
-        imageUrl: "http://sliet.ac.in/wp-content/uploads/avatars/6/605c66bd3e3bc-bpfull.jpg",
+        imageUrl: "https://i.ibb.co/3f0hFgN/Dr-Manoj-Sachan.jpg",
         availableSlots: ["9:00 AM", "1:00 PM", "3:00 PM"]
     },{
         name: "Dr. Gurjinder Kaur",
         position: "Associate Professor",
         expertise: "B.E,M.S,Ph.D",
-        imageUrl: "http://sliet.ac.in/wp-content/uploads/avatars/14/5f2245e7a7f1bde5dee5bda48e09d411-bpfull.jpg",
+        imageUrl: "https://i.ibb.co/GMnfsPJ/dr-gurijinder-karu.jpg",
         availableSlots: ["9:00 AM", "1:00 PM", "3:00 PM"]
     }
     ,{
         name: "Dr. Amar Nath",
         position: "Assistant Professor",
         expertise: "B.tech,M.tech,Ph.D",
-        imageUrl: "http://sliet.ac.in/wp-content/uploads/avatars/467/66b8bb350cc46-bpfull.jpg",
+        imageUrl: "https://i.ibb.co/dkBkxgX/Dr-Amar-Nath.jpg",
         availableSlots: ["9:00 AM", "1:00 PM", "3:00 PM"]
     },{
         name: "Dr. Jagdeep Singh",
         position: "Assistant Professor",
         expertise: "B.tech,M.tech,Ph.D",
-        imageUrl: "http://sliet.ac.in/wp-content/uploads/avatars/466/66f81ca30f4ff-bpfull.jpg",
+        imageUrl: "https://imgbb.com/][img]https://i.ibb.co/gjrT61Q/Dr-Jagdeep-Singh.jpg[/img][/url",
         availableSlots: ["9:00 AM", "1:00 PM", "3:00 PM"]
     },{
         name: "Dr. Manminder Singh",
@@ -52,43 +52,43 @@
         name: "Dr. Preetpal Kaur Buttar",
         position: "Assistant Professor",
         expertise: "Ph.D",
-        imageUrl: "http://sliet.ac.in/wp-content/uploads/avatars/349/632d98c75d80f-bpfull.jpg",
+        imageUrl: "https://i.ibb.co/Vj1GrX3/Dr-Preetpal-Kaur-Buttar.jpg",
         availableSlots: ["9:00 AM", "1:00 PM", "3:00 PM"]
     },{
         name: "Dr. Tajinder Singh",
         position: "Assistant Professor",
         expertise: "Ph.D",
-        imageUrl: "http://sliet.ac.in/wp-content/uploads/avatars/468/64e3532d30fac-bpfull.jpg",
+        imageUrl: "https://i.ibb.co/QQK5wG6/Dr-Tajinder-Singh.jpg",
         availableSlots: ["9:00 AM", "1:00 PM", "3:00 PM"]
     },{
         name: "Dr.Utkarsh",
         position: "Assistant Professor",
         expertise: "B.Tech,M.Tech,Ph.D",
-        imageUrl: "http://sliet.ac.in/wp-content/uploads/avatars/493/6165aace595a1-bpfull.jpg",
+        imageUrl: "https://i.ibb.co/vLGWK5n/Dr-Utkarsh.jpg",
         availableSlots: ["9:00 AM", "1:00 PM", "3:00 PM"]
     },{
         name: "Dr. VINOD KUMAR VERMA",
         position: "Assistant Professor",
         expertise: "Ph.D",
-        imageUrl: "http://sliet.ac.in/wp-content/uploads/avatars/17/28b95ba0ed455dca378c1ada58307bf6-bpfull.jpg",
+        imageUrl: "https://i.ibb.co/YRDVbGx/Dr-VINOD-KUMAR-VERMA.png",
         availableSlots: ["9:00 AM", "1:00 PM", "3:00 PM"]
     },{
         name: "Jaspal Singh",
         position: "Assistant Professor",
         expertise: "M.E,Ph.D",
-        imageUrl: "http://sliet.ac.in/wp-content/uploads/avatars/36/e0a878e8162fd75cb5eb6929d98df5c1-bpfull.jpg",
+        imageUrl: "https://i.ibb.co/ss72g2d/Jaspal-Singh.jpg",
         availableSlots: ["9:00 AM", "1:00 PM", "3:00 PM"]
     },{
         name: "Rahul Gautam",
         position: "Assistant Professor",
         expertise: "B.tech,M.Tech,Ph.D",
-        imageUrl: "http://sliet.ac.in/wp-content/uploads/avatars/350/666bf221c60a0-bpfull.jpg",
+        imageUrl: "https://i.ibb.co/LZ4qYCk/Rahul-Gautam.jpg",
         availableSlots: ["9:00 AM", "1:00 PM", "3:00 PM"]
     },{
         name: "Dr. Jatinder Pal Singh",
         position: "Assistant Professor(Contract)",
         expertise: "M.E,Ph.D",
-        imageUrl: "http://sliet.ac.in/wp-content/uploads/avatars/344/64edd5a68d11b-bpfull.jpg",
+        imageUrl: "https://i.ibb.co/5j97Ydk/Dr-Jatinder-Pal-Singh.jpg",
         availableSlots: ["9:00 AM", "1:00 PM", "3:00 PM"]
     },{
         name: "Sukhpreet Singh",
